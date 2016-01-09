0584cb0301997ab1b9919dc3218846fbc657560f
tombaru <flashfair@gmail.com>
2016-01-09 19:10:52 +0300

    add fix Portfolio, Index, Contact page

--- app/js/main.js
+++ app/js/main.js
@@ -10,8 +10,12 @@ var app = (function(){
   };
 
   var showText =function(){
-    console.log($(this).val().substr(12));
-    $('.fake-upload').html($(this).val().substr(12));
+    var fileName = $(this).val().substr(12);
+    var fakeInput = ($('.fake-upload .input'));
+    var fakeInput = fakeInput[0];
+    fakeInput.placeholder = fileName;
+
+    // $('.fake-upload').children('input').placeholder = ($(this).val().substr(12));
   };
 
 
